5236b79b1c61fda89c7e29d804c0446f9dc471e2
Glitch (charm-city-circulator) <none>
2020-12-17 23:49:53 +0000

    👽💸 Checkpoint ./src/sass/layout/_site-grid-stops.scss:22028170/7254 ./src/stops.html:22028170/412 ./src/js/accordian.js:22028170/6

--- src/js/accordian.js
+++ src/js/accordian.js
@@ -2,7 +2,7 @@
 
 (function() {
     // Get all the H2 headings
-    const headings = document.querySelectorAll(".js-accordion h5");
+    const headings = document.querySelectorAll(".js-accordion h3");
   
     for (let heading of headings) {
       // Create button element
